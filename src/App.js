89a50cadc17d02f570b2fc1a5fffeca0a9e83b9c
asapsahil <87446483+asapsahil@users.noreply.github.com>
2022-03-23 11:25:32 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+
+import './App.css';
+import Header from "./Components/header"
+import Home from "./Components/home"
+
+function App() {
+  return (
+    <div className="App">
+      <Header/> 
+     <Home />
+    </div>
+  );
+}
+
+export default App;
